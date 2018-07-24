374edacee3b7033af63fbdf77502df67b40d10d5
Kir Belevich <kir@belevi.ch>
2018-07-24 19:05:44 +0200

    ➕ lexer-pairs: handle double quotes

--- packages/lexer-pairs/src/index.ts
+++ packages/lexer-pairs/src/index.ts
@@ -1,10 +1,16 @@
 import {
   TLines,
   T_LEFT_PARENTHESIS,
-  T_RIGHT_PARENTHESIS
+  T_RIGHT_PARENTHESIS,
+  // T_SINGLE_QUOTE,
+  T_DOUBLE_QUOTE
+  // T_BACKTICK
 } from '@txt/tokenizer/src/'
 
 export const T_PARENTHESES = 'PARENTHESES'
+// export const T_SINGLE_QUOTES = 'SINGLE_QUOTES'
+export const T_DOUBLE_QUOTES = 'DOUBLE_QUOTES'
+// export const T_BACKTICKS = 'BACKTICKS'
 
 export type TPair = {
   from: {
@@ -21,7 +27,8 @@ export type TPairs = TPair[]
 
 export const lexerPairs = (lines: TLines): TPairs => {
   const pairs = {
-    [T_PARENTHESES]: []
+    [T_PARENTHESES]: [],
+    [T_DOUBLE_QUOTES]: []
   }
   const result = []
 
@@ -31,17 +38,38 @@ export const lexerPairs = (lines: TLines): TPairs => {
     for (let ti = 0; ti < tokens.length; ti++) {
       const token = tokens[ti]
 
-      if (token.type === T_LEFT_PARENTHESIS) {
+      if (
+        token.type === T_LEFT_PARENTHESIS &&
+        pairs[T_DOUBLE_QUOTES].length === 0
+      ) {
         pairs[T_PARENTHESES].push({ x: ti, y: li })
         continue
       }
 
-      if (token.type === T_RIGHT_PARENTHESIS && pairs[T_PARENTHESES].length > 0) {
+      if (
+        token.type === T_RIGHT_PARENTHESIS &&
+        pairs[T_PARENTHESES].length > 0 &&
+        pairs[T_DOUBLE_QUOTES].length === 0
+      ) {
         result.push({
           type: T_PARENTHESES,
           open: pairs[T_PARENTHESES].pop(),
           close: { x: ti, y: li }
         })
+        continue
+      }
+
+      if (token.type === T_DOUBLE_QUOTE && pairs[T_DOUBLE_QUOTES].length > 0) {
+        result.push({
+          type: T_DOUBLE_QUOTES,
+          open: pairs[T_DOUBLE_QUOTES].pop(),
+          close: { x: ti, y: li }
+        })
+        continue
+      }
+
+      if (token.type === T_DOUBLE_QUOTE) {
+        pairs[T_DOUBLE_QUOTES].push({ x: ti, y: li })
         continue
       }
     }
--- packages/lexer-pairs/test/index.ts
+++ packages/lexer-pairs/test/index.ts
@@ -1,7 +1,11 @@
 import test from 'tape'
 import { tokenizer } from '@txt/tokenizer/src/'
 
-import { lexerPairs, T_PARENTHESES } from '../src/'
+import {
+  lexerPairs,
+  T_PARENTHESES,
+  T_DOUBLE_QUOTES
+} from '../src/'
 
 test('lexer-pairs', (t) => {
   const tokenize = tokenizer()
@@ -51,6 +55,34 @@ test('lexer-pairs', (t) => {
       }
     ],
     'nested parentheses'
+  )
+
+  t.deepEqual(
+    lexerPairs(
+      tokenize('"foo"')
+    ),
+    [
+      {
+        type: T_DOUBLE_QUOTES,
+        open: { x: 0, y: 0 },
+        close: { x: 2, y: 0 }
+      }
+    ],
+    'double quotes'
+  )
+
+  t.deepEqual(
+    lexerPairs(
+      tokenize('"fo(o)"')
+    ),
+    [
+      {
+        type: T_DOUBLE_QUOTES,
+        open: { x: 0, y: 0 },
+        close: { x: 5, y: 0 }
+      }
+    ],
+    'parentheses inside of double quotes'
   )
 
   t.end()
