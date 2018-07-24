93f213ef30487201425b230e266f9cbb3b7bb20b
Kir Belevich <kir@belevi.ch>
2018-07-24 22:42:22 +0200

    ➕ find-current-token-index: initial implementation

---
+++ packages/find-current-token-index/src/index.ts
@@ -0,0 +1,19 @@
+// TODO: transpile back on build
+import { TLine } from '@txt/tokenizer/src/'
+import binarySearch from 'bsc'
+
+const findCurrentTokenIndex = (line: TLine, x: number) => {
+  return binarySearch(line, x, (key, token) => {
+    if (key < token.from && key < token.to) {
+      return -1
+    }
+
+    if (key > token.from && key > token.to) {
+      return 1
+    }
+
+    return 0
+  })
+}
+
+export default findCurrentTokenIndex
---
+++ packages/find-current-token-index/test/index.ts
@@ -0,0 +1,35 @@
+import test from 'tape'
+import { tokenizer } from '@txt/tokenizer'
+
+import findCurrentTokenIndex from '../src/'
+
+test('find-current-token-index', (t) => {
+  const tokenize = tokenizer()
+  const tokens = tokenize('hello world')[0]
+
+  t.deepEqual(
+    findCurrentTokenIndex(tokens, 2),
+    0,
+    'should find first token'
+  )
+
+  t.deepEqual(
+    findCurrentTokenIndex(tokens, 5),
+    1,
+    'should find second token'
+  )
+
+  t.deepEqual(
+    findCurrentTokenIndex(tokens, 9),
+    2,
+    'should find third token'
+  )
+
+  t.deepEqual(
+    findCurrentTokenIndex(tokens, 20),
+    -1,
+    'should return -1 if nothing has been found'
+  )
+
+  t.end()
+})
